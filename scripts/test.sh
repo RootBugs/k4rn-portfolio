5923e3c72fbd7ea23dcc2c55cf5f77e8c9eda507
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-16 15:46:58 +0000

    update deps: formatting in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
+# // changelog: add_import — processChangelog
+# // buffer: add_import — setupBuffer
 # // animation: add_import — createAnimation
 # checked: handler
 # // query: add_constant — initQuery
@@ -302,12 +304,6 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
-# // compress: add_conditional — setupCompress
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
 # // spy: add_constant — buildSpy
