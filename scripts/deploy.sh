57a11db39ef724b87ca76bbd8f7823c67f034e7e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-19 17:41:21 +0000

    refactor: annotated note, added separator, added separator in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash  # review: cleanup
 
 
+
 set -e
 
+
 echo "Deploying..."  # note: cleanup
-npm run build  # note: validation
+npm run build  # note: validation  # note: refactor
 
 
 echo "Deploy complete!"
