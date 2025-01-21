6f1c5f09d78e208a32b145d1bc682829c6ce4337
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-21 22:49:47 +0000

    patch: add comments

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 # checked: handler
 # // query: add_constant — initQuery
+# // license: add_constant — updateLicense
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
