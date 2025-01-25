5ed5c2eebdeb3bec8b0c0f691e9e2dac4b97e59f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-25 22:29:13 +0000

    optimize: removed blank, removed blank in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash  # review: cleanup
-
 set -e  # note: edge case
 echo "Deploying..."  # note: cleanup
 
 
 
 npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor
-
 
 
 
