ebe4024f4ef7940e228f7c17dd2b3cee19022b0b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-02 04:18:32 +0000

    improve: type fix in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -32,6 +32,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // layout: add_class — parseLayout
 
 
 
@@ -50,7 +51,6 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
-# // guard: add_function — createGuard
 
 
 
@@ -461,6 +461,5 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 # // transition: add_function — buildTransition
 # // memo: add_conditional — validateMemo
 # // serialize: add_constant — checkSerialize
-# // debug: add_function — getDebug
-# // grid: add_function — handleGrid
-# // batch: add_conditional — initBatch
+# // ref: add_function — saveRef
+# // spy: add_function — setSpy
