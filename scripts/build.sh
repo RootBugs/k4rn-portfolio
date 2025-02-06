261d17a71f8ee66fcf77cba25055f461a84e597c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-06 13:12:53 +0000

    refactor: added separator, annotated review, added spacing in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,15 @@
 # checked: logic
+
 set -e
 
 
 
-echo "Building project..."  # TODO: performance
+echo "Building project..."  # TODO: performance  # review: performance
 
 
 
 npm run build
+
 
 
 echo "Build complete!"  # HACK: refactor
