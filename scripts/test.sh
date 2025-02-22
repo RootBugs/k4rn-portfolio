ff150ff66888996819983cc40ecf8b7d1237f8cb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-22 10:45:11 +0000

    clean: annotated TODO, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,7 +16,9 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor
 
 
 
-echo "Running tests..."  # TODO: validation
+
+
+echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
 npm test
