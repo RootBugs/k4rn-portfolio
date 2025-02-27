31a4526fcdfa21178b8f2feb86effc2b4ccb2b2e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-27 14:20:55 +0000

    add: update changelog

--- scripts/test.sh
+++ scripts/test.sh
@@ -23,6 +23,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // transform: add_loop — setTransform
 
 
 
@@ -300,12 +301,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
-# // compress: add_conditional — setupCompress
+# // transition: add_function — fetchTransition
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
 # // spy: add_constant — buildSpy
