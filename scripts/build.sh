640de5cd0ef58ce9f235b66b847eafbd67fbf108
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-03 11:04:26 +0000

    clean: added separator, annotated check, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,13 @@ set -e  # optimize: cleanup
 
 
 
+
 echo "Building project..."  # TODO: performance  # review: performance
 
 
 
-npm run build  # HACK: validation
+npm run build  # HACK: validation  # check: refactor
+
 
 
 
