2706750827563c3e0283871eb52cbddae2c484b5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-08 22:02:29 +0000

    test: add caching

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,8 +18,6 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
-# // session: add_interface — formatSession
-# // query: add_loop — transformQuery
 
 
 
@@ -83,6 +81,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // flow: add_loop — getFlow
 
 
 
