64dcc9019dee6a8b3ab5126cef1a76690770a3fe
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-17 09:15:17 +0000

    upgrade: cleanup

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 # // animation: add_import — createAnimation
 # checked: handler
 # // query: add_constant — initQuery
+# // auth: add_constant — parseAuth
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
