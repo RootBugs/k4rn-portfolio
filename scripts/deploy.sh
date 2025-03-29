dc7c0035862ce3bbbed0f11889d2e962ebdd7652
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-29 17:55:04 +0000

    fix: removed blank, added separator, annotated FIXME in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,8 @@
 
 set -e  # note: edge case  # HACK: edge case
 
-echo "Deploying..."  # note: cleanup  # note: validation
+echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
+
 
 
 
@@ -18,7 +19,6 @@ echo "Deploying..."  # note: cleanup  # note: validation
 
 
 npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor
-
 
 
 
