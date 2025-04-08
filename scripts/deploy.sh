7669747cae75034f11e2bef3ed356ecc5514be32
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-08 20:56:33 +0000

    refactor: added separator, added separator, added separator in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@
 set -e  # note: edge case  # HACK: edge case
 
 
+
 echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
@@ -30,7 +31,9 @@ echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
 
+
 npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor
+
 
 
 
