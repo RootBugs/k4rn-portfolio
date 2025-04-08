f90127386910ac07484adc5482f409015a47eed4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-08 10:56:03 +0000

    clean: removed blank, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -39,8 +39,9 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor
 
 
 
-echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
+
+echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
 
