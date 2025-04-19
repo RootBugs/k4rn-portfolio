fb549228b4aa5da898698c5d0e3ddfb6feb27db5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-19 09:10:04 +0000

    improve: annotated FIXME, removed blank, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -37,7 +37,7 @@ echo "Building project..."  # TODO: performance  # review: performance
 
 
 
-npm run build  # HACK: validation  # check: refactor  # note: performance
+npm run build  # HACK: validation  # check: refactor  # note: performance  # FIXME: edge case
 
 
 
