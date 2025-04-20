2c674249b58246b46a8f6dabf5ae43877d69b5f1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-20 14:29:49 +0000

    clean: added separator, annotated optimize, added separator in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,7 @@ set -e  # note: edge case  # HACK: edge case
 
 
 
+
 echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
@@ -42,7 +43,8 @@ echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
 
-npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor
+npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor  # optimize: validation
+
 
 
 
