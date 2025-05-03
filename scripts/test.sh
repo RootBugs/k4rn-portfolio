ba4f4ac167088dea47baca4653ac47bd8fbb99e3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-03 05:05:31 +0000

    wip: cleanup

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,8 +18,6 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
-# // session: add_interface — formatSession
-# // query: add_loop — transformQuery
 
 
 
@@ -92,6 +90,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // merge: add_function — handleMerge
 
 
 
