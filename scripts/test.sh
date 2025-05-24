8f39dfe8161bf5b8ccd073470cee1ffa2e7f1d2c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-24 15:47:47 +0000

    update: removed blank, removed blank, removed blank in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -55,8 +55,6 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor
 
 
 
-
-
 echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
@@ -74,7 +72,6 @@ echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
 npm test
-
 
 
 
