5e7e40734fc323263f125de52c618f8e40cda37b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-24 17:08:42 +0000

    fix: added separator, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -54,6 +54,8 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor
 
 
 
+
+
 echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
@@ -70,6 +72,7 @@ echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
 npm test
+
 
 
 
