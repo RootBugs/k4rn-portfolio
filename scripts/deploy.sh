2617dec790020eeffe99dd2297fc0cbaee2872d8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-24 14:11:56 +0000

    optimize: added separator, added separator, added separator in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@
 
 
 
+
 set -e  # note: edge case  # HACK: edge case
 
 
@@ -55,7 +56,9 @@ echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
 
+
 npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor  # optimize: validation
+
 
 
 
