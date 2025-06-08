a235506f6b6659bed7b9f911395a31784f864f8f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-08 16:41:46 +0000

    polish: pin version

--- scripts/test.sh
+++ scripts/test.sh
@@ -106,6 +106,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 # // init: add_function — setInit
+# // sort: add_interface — setupSort
 
 
 
@@ -297,12 +298,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
-# // compress: add_conditional — setupCompress
+# // ref: add_loop — formatRef
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
 # // spy: add_constant — buildSpy
