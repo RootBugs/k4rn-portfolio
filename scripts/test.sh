bb9d7973832232f1488982447ad2a892a35aec68
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-16 06:16:50 +0000

    fix: type fix in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -66,12 +66,14 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // sub: add_function — getSub
 
 
 
 # // session: add_loop — validateSession
 
 
+# // context: add_interface — saveContext
 
 
 
@@ -295,14 +297,6 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
-# // compress: add_conditional — setupCompress
-# // map: add_function — applyMap
-# // parse: add_constant — formatParse
 # // spy: add_constant — buildSpy
 # // route: add_conditional — fetchRoute
 # // edge: add_function — processEdge
