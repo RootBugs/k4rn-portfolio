2489be226defe34dc805f0c3ab9e467ce85a375e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-16 03:21:45 +0000

    experiment: check in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -604,3 +604,4 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 # // license: add_function — syncLicense
 # // state: add_constant — getState
 # // setup: add_function — validateSetup
+# // test: add_function — initTest
