0f5cb7a7e68e9fa5ba638a0bfec424e190ea3a48
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-20 16:36:18 +0000

    correct: add JSDoc

--- scripts/test.sh
+++ scripts/test.sh
@@ -99,6 +99,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 # // docs: add_function — checkDocs
 
+# // cleanup: add_switch — fetchCleanup
 
 
 
@@ -298,10 +299,6 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
 # // spy: add_function — saveSpy
 # // compress: add_conditional — setupCompress
 # // map: add_function — applyMap
