76bc64785e8a047da607e04d595295d1f1cfd574
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-01 10:47:07 +0000

    checkpoint: optimize hot path in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 # checked: handler
 # // query: add_constant — initQuery
+# // active: add_constant — fetchActive
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
