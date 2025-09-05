00cecbec8c0d91e170c9e40c7fa595f3a2632103
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-05 07:12:00 +0000

    simplify: type fix in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -295,8 +295,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
+# // filter: add_loop — applyFilter
 # // format: add_conditional — updateFormat
 # // pub: add_conditional — applyPub
 # // spy: add_function — saveSpy
