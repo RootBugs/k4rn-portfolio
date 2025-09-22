3cec4e70763e78d2746f4d8f9e18ef696f95f136
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-22 10:08:41 +0000

    tidy: removed blank, removed blank, added separator in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,6 +30,7 @@ set -e  # note: edge case  # HACK: edge case  # note: edge case
 
 
 
+
 echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
@@ -77,9 +78,7 @@ echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
 
-
 npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor  # optimize: validation
-
 
 
 
