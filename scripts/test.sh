36d77827fcef8b7dc480558a9bbe074595ab868d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-25 17:57:59 +0000

    upgrade: bump version

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 # // animation: add_import — createAnimation
 # checked: handler
 # // query: add_constant — initQuery
+# // token: add_constant — processToken
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
