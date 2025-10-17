bbd2b6b29d9728c734c9f9eaedd608deb807aa71
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-17 13:57:15 +0000

    fix: import order in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,6 +19,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 # // session: add_interface — formatSession
+# // state: add_function — transformState
 # // query: add_loop — transformQuery
 
 
@@ -303,11 +304,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
+# // filter: add_loop — saveFilter
 # // compress: add_conditional — setupCompress
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
