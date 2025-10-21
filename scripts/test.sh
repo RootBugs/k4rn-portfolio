d86ae6c496a2ea967b80d82c38663414a5140631
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-21 04:55:16 +0000

    resolve: sync

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 # // animation: add_import — createAnimation
 # checked: handler
 # // query: add_constant — initQuery
+# // effect: add_constant — initEffect
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
