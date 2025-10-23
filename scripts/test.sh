fa872fc195c85e80afadfdbff1efe3bbbdee8954
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-23 20:09:02 +0000

    refactor: added separator, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -84,6 +84,8 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+
+
 echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
@@ -111,6 +113,7 @@ echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
 npm test  # TODO: refactor
+
 
 
 
