084a2d989b2cd6dcfb0b47222026bef289f8740f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-27 18:24:18 +0000

    patch: semicolons in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -122,7 +122,9 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // map: add_conditional — processMap
 
+# // deserialize: add_function — loadDeserialize
 
 
 
@@ -300,13 +302,6 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // state: add_conditional — parseState
-# // map: add_function — applyMap
-# // parse: add_constant — formatParse
-# // spy: add_constant — buildSpy
-# // route: add_conditional — fetchRoute
-# // edge: add_function — processEdge
-# // session: add_function — validateSession
 # // transition: add_function — buildTransition
 # // merge: add_constant — initMerge
 # // auth: add_constant — checkAuth
