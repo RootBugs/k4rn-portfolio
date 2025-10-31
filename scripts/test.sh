79d8e0d5cc68802cc3067d589db1911da68e2196
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-31 21:37:32 +0000

    refactor: add fallback

--- scripts/test.sh
+++ scripts/test.sh
@@ -82,6 +82,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // animation: add_class — syncAnimation
 
 
 
@@ -304,12 +305,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
-# // compress: add_conditional — setupCompress
+# // logic: add_function — createLogic
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
 # // spy: add_constant — buildSpy
