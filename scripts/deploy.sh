a52598ea4ecd6a107ec27d2dd4ff058c26d4e84a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-02 08:47:40 +0000

    update: removed blank, added separator, added separator in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,7 @@ set -e  # note: edge case  # HACK: edge case  # note: edge case
 
 
 
+
 echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
@@ -85,8 +86,8 @@ echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
 
-npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor  # optimize: validation
 
+npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor  # optimize: validation
 
 
 
