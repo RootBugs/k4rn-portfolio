c1ab0cda7e688533baa288de91c2007d25e32a7e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-05 13:09:09 +0000

    address: quotes in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -296,11 +296,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
+# // validate: add_loop — parseValidate
 # // compress: add_conditional — setupCompress
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
