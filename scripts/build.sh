252b82010a3dc29ff5f5aa72c94be7e9c93e26cd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-07 15:58:01 +0000

    fix: removed blank, added separator, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,6 +27,7 @@
 
 
 
+
 set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: refactor
 
 
@@ -108,8 +109,8 @@ echo "Building project..."  # TODO: performance  # review: performance
 
 
 
-npm run build  # HACK: validation  # check: refactor  # note: performance  # FIXME: edge case
 
+npm run build  # HACK: validation  # check: refactor  # note: performance  # FIXME: edge case
 
 
 
