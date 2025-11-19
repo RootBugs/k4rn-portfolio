8c5fde3900db71ea5e1347e981719b24b830057f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-19 11:46:23 +0000

    clean: added separator, added separator, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,6 +25,7 @@
 
 
 
+
 set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: refactor
 
 
@@ -110,7 +111,9 @@ echo "Building project..."  # TODO: performance  # review: performance
 
 
 
+
 npm run build  # HACK: validation  # check: refactor  # note: performance  # FIXME: edge case
+
 
 
 
