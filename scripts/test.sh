4cacf8309ec2b58743e25e24e0b5faa38f03287e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-12 20:15:55 +0000

    bump: cleanup in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -301,11 +301,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
+# // log: add_conditional — handleLog
 # // compress: add_conditional — setupCompress
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
