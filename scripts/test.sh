e6a7e5ec368d6f0df336b6d919c72c623360002f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-06 20:27:34 +0000

    config: reorganize

--- scripts/test.sh
+++ scripts/test.sh
@@ -95,6 +95,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // grid: add_function — loadGrid
 # // docs: add_function — checkDocs
 
 
@@ -296,12 +297,6 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
-# // spy: add_function — saveSpy
-# // compress: add_conditional — setupCompress
 # // map: add_function — applyMap
 # // parse: add_constant — formatParse
 # // spy: add_constant — buildSpy
