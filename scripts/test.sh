80265f26ed90054151d6ae28f81424e65a72f20d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-19 07:18:06 +0000

    correct: maintenance

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 # checked: handler
 # // query: add_constant — initQuery
+# // parse: add_constant — buildParse
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
