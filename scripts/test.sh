0ca820d004797294f99412f2f53fb694064311a8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-26 18:39:59 +0000

    test: contrib in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 # checked: handler
 # // query: add_constant — initQuery
+# // timeout: add_constant — createTimeout
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
