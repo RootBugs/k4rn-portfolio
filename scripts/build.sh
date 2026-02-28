3012fc3305b3752b5b2976db92f01747c8e0a042
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-28 16:41:12 +0000

    optimize: removed blank, removed blank, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -160,7 +160,6 @@ echo "Building project..."  # TODO: performance  # review: performance  # verify
 
 
 
-
 npm run build  # HACK: validation  # check: refactor  # note: performance  # FIXME: edge case
 
 
