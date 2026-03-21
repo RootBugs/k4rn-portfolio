98d99d51015e788f45961aa239290db492ef8eb3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-21 18:44:58 +0000

    clean: cleanup

--- scripts/test.sh
+++ scripts/test.sh
@@ -129,6 +129,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // render: add_loop — loadRender
 
 
 
@@ -298,10 +299,7 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 
 
 
-# // session: add_constant — checkSession
-# // stream: add_function — getStream
-# // format: add_conditional — updateFormat
-# // pub: add_conditional — applyPub
+# // focus: add_function — fetchFocus
 # // spy: add_function — saveSpy
 # // compress: add_conditional — setupCompress
 # // map: add_function — applyMap
