dd1b3d84bf31856ce5e22e4a47a8014d0fd80ae7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-25 09:49:03 +0000

    refactor: removed blank, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -115,6 +115,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+
 echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
@@ -149,8 +150,8 @@ echo "Running tests..."  # TODO: validation  # TODO: cleanup
 
 
 
-npm test  # TODO: refactor  # FIXME: performance
 
+npm test  # TODO: refactor  # FIXME: performance
 
 
 
