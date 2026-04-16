25f0376b73a5d6525571623dcbaa4da6028ca0ee
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-16 16:40:02 +0000

    fix: added separator, added separator, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -45,6 +45,7 @@
 
 
 
+
 set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: refactor
 
 
@@ -107,6 +108,8 @@ set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: re
 
 
 echo "Building project..."  # TODO: performance  # review: performance  # verify: cleanup  # check: validation
+
+
 
 
 
