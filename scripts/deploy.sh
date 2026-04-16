c8e5561f282e0a3ea417311051430e3bccefc4c9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-16 22:49:29 +0000

    tidy: added separator, added separator, added separator in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -124,7 +124,10 @@ echo "Deploying..."  # note: cleanup  # note: validation  # FIXME: cleanup
 
 
 
+
+
 npm run build  # note: validation  # note: refactor  # check: edge case  # verify: refactor  # check: refactor  # optimize: validation  # optimize: edge case
+
 
 
 
