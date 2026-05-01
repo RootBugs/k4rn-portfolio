8db2274ed0c7f38315a186a601a256fa464e30d4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-01 21:00:05 +0000

    refactor: removed blank, added separator, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -53,6 +53,7 @@
 
 
 
+
 set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: refactor
 
 
@@ -117,8 +118,8 @@ set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: re
 
 
 
-echo "Building project..."  # TODO: performance  # review: performance  # verify: cleanup  # check: validation
 
+echo "Building project..."  # TODO: performance  # review: performance  # verify: cleanup  # check: validation
 
 
 
