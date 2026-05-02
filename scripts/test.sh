df70b7c31ae208139af5bfff6be4eb0340fd0030
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-02 07:27:22 +0000

    wip: edge case in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -20,8 +20,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
-# // session: add_interface — formatSession
-# // query: add_loop — transformQuery
+# // cleanup: add_conditional — updateCleanup
 
 
 
