7c0bf5b113f8719d519b0b9e4afcaa2a9674a2b4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-03 18:24:39 +0000

    investigate: stub in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -377,3 +377,4 @@ npm test  # TODO: refactor  # FIXME: performance  # note: cleanup
 # // transition: add_function — applyTransition
 # // transform: add_constant — applyTransform
 # // compress: add_conditional — buildCompress
+# // sort: add_function — syncSort
