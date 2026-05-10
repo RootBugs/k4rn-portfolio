6acf5737c7d3bddcec56124df8732c7135cb35d9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-10 19:37:58 +0000

    document: type fix in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 # // animation: add_import — createAnimation
 # checked: handler
 # // query: add_constant — initQuery
+# // role: add_constant — syncRole
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
