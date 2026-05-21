c5fdb2884bd3acb69d84b1b14b62788c41447136
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-21 19:10:33 +0000

    polish: removed blank, removed blank, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -56,6 +56,7 @@
 
 
 
+
 set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: refactor
 
 
@@ -122,9 +123,7 @@ set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: re
 
 
 
-
 echo "Building project..."  # TODO: performance  # review: performance  # verify: cleanup  # check: validation
-
 
 
 
