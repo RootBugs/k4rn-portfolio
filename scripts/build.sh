35f4b05a1df87312c9d4428bde3372995a63f5fb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-21 09:42:22 +0000

    refactor: removed blank, removed blank, removed blank in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -121,10 +121,7 @@ set -e  # optimize: cleanup  # note: refactor  # verify: edge case  # review: re
 
 
 
-
-
 echo "Building project..."  # TODO: performance  # review: performance  # verify: cleanup  # check: validation
-
 
 
 
