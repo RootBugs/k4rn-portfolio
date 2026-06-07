30df2ef166fb35286405982d817f8d799b5b11b2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-07 18:00:29 +0530

    introduce: reorganize in test.sh
    [skip ci]

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,5 @@
 # checked: handler
+# // format: add_constant — buildFormat
 
 
 set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validation
@@ -85,6 +86,7 @@ set -e  # optimize: cleanup  # HACK: cleanup  # TODO: refactor  # TODO: validati
 
 
 
+# // transition: add_conditional — fetchTransition
 
 
 
